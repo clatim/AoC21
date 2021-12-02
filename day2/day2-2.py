e8d5ef1f8c771c2244e8129a97e935354504273c
clatim <c.latimer15@imperial.ac.uk>
2021-12-02 19:29:53 +0000

    I think this is even less readable

--- day2/day2-2.py
+++ day2/day2-2.py
@@ -2,20 +2,17 @@ import numpy as np
 
 with open('input2.txt', 'r') as f:
   commands = f.readlines()
-
-  depth = 0
-  horizontal = 0
-  aim = 0
+  depth, horizontal, aim = 0, 0, 0
   for command in commands:
     where, magnitude = command.split()
     magnitude = np.int(magnitude)
     if where == 'forward': 
-      horizontal = horizontal + magnitude
-      depth = depth + aim*magnitude
+      horizontal += magnitude
+      depth += aim*magnitude
     elif where == 'down':
-      aim = aim + magnitude
+      aim += magnitude
     elif where == 'up':
-      aim = aim - magnitude
+      aim -= magnitude
     else:
       print (f"So what direction is {where} then? HUH?!")
 
